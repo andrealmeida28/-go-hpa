ea3facee3e6ef7b28a3f435317bc7628e50e2cf9
andrealmeida28 <andrealmeida28@yahoo.com.br>
2020-08-31 21:20:39 -0300

    Alteracao no codigo

--- src/greeting.go
+++ src/greeting.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	x := 0.0001
+	for i := 0; i <= 1000000; i++ {
+		x = math.Sqrt(x)
+	}
 	fmt.Fprintf(w, "%s", greeting("Code.education Rocks!"))
 }
 
